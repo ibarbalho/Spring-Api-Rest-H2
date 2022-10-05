623741ce5ecff3ff38fa23fe72500307d5d163ac
Ittalo Barbalho <ittalo.renan@gmail.com>
2022-10-05 15:40:04 +0200

    sql file
    preload content into h2

---
+++ h2databaseapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+/*CREATE TABLE tbl_customer (
+	id LONG NOT NULL AUTO_INCREMENT,
+	name VARCHAR(255),
+	age LONG,
+	location VARCHAR(255),
+	creatAt TIMESTAMP,
+	updateAt TIMESTAMP,
+	PRIMARY KEY(id)
+);*/
+
+//INSERT INTO tbl_customer (name, age, location) values ('Ittalo', '34', 'Germany');
